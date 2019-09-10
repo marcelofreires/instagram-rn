473c0a95b6a2c6b60f9d62a85ac38d0a8e51bc5a
Marcelo Freire <marcelo.freire.silva@gmail.com>
2019-09-09 23:32:39 -0300

    Instalar e configurar eslint e babel-root-import
    Foi instalado e configurado o eslint baseado na style guide do Airbnb,
    sobrescrevendo algumas regras. Também foi instalado o
    babel-plugin-root-import para configurar o src/ como diretório base
    para realizar as importações. Foi adicionado um .editorconfig para
    definir as mesmas regras de formatação em todos os editores de texto.
    Também foi criado um jsconfig.json para habilitar o intellisense do
    babel-plugin-root-import ao VS Code.

---
+++ src/pages/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text } from 'react-native'
+import Test from '~/components/test'
+
+const Page = () => (
+  <>
+    <Text>Page</Text>
+    <Test />
+  </>
+)
+
+export default Page
